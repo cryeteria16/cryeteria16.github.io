9808579fb5ac9ccbe819cf62adfa46e314c134ed
cryeteria <58374344+cryeteria16@users.noreply.github.com>
2026-09-03 20:00:09 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,11 +3,10 @@ const ASSETS = ['/', '/index.html', '/lair.html', '/admin.html', '/manifest.json
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+  // Pass-through fetch for clean network requests
 });
 
 self.addEventListener('push', (event) => {
